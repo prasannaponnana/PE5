f54c784cd6be91155cd788f090d3347e90d57e52
prasanna <prasannakumari879913@gmail.com>
2019-05-21 09:13:43 +0530

    pe5

--- src/main/java/com/stackroute/Reolaceboolean.java
+++ src/main/java/com/stackroute/Reolaceboolean.java
@@ -8,7 +8,7 @@ public class Reolaceboolean {
     public Map<String,Boolean> Map(String[] s) {
             Map<String, Boolean> boo = new HashMap<>();
             for (String p : s) {
-                if (boo.containsKey(word)) {
+                if (boo.containsKey(p)) {
                     boo.put(p, true);
                 } else {
                     boo.put(p, false);
